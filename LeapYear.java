af31cc816b3f790756df4123bf473e72bb4851ba
Harmanjot-Singh <53915816+Harmanjot-Singh@users.noreply.github.com>
2019-08-15 14:10:20 +0530

    Add files via upload

---
+++ LeapYear.java
@@ -0,0 +1,35 @@
+public class LeapYear {
+    public static boolean isLeapYear(int year) {
+
+      /*  if ((year >= 1 && year <= 9999) && (year % 4 == 0) && (year % 100 == 0 && year % 400 == 0) ) {
+            return true;
+        } else if (year < 0) {
+            return false;
+        } else
+            return false;*/
+      /*if (year >= 1 && year <= 9999){
+          if(year % 4 == 0){
+              if (year % 100 == 0){
+                  if(year % 400 == 0){
+                      return true;
+                  } else return false;
+              } else return true;
+
+          } else return false;
+      } else if (year < 0) {
+          return false;
+      } else return false;
+    }
+}*/
+        if (year >= 1 && year <= 9999) {
+         if (year % 4 ==0){
+            return true;
+        } else if (year % 100 == 0){
+            return true;
+        } else if (year % 400 == 0){
+            return true;
+        } else return false;
+        }
+
+    }
+}
---
+++ main.java
@@ -0,0 +1,6 @@
+public class main {
+    public static void main(String[] args) {
+        boolean leapYear= LeapYear.isLeapYear(2001);
+        System.out.println(leapYear);
+    }
+}
